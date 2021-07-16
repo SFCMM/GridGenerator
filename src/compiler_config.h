9d709c8606080f3b265e98a85af6c0ad86aef9e9
svchb <berger.sven@gmail.com>
2021-07-16 20:13:42 +0200

    remove unused code add compiler_config.h

---
+++ src/compiler_config.h
@@ -0,0 +1,12 @@
+#ifndef GRIDGENERATOR_COMPILER_CONFIG_H
+#define GRIDGENERATOR_COMPILER_CONFIG_H
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER) && !defined(__clang__) && !defined(__PGI) && !defined(_CRAYC)      \
+    && !defined(_SX)
+#define GCC_COMPILER
+#elif defined(__clang__)
+#define CLANG_COMPILER
+#else
+#error Unsupported compiler
+#endif
+
+#endif // GRIDGENERATOR_COMPILER_CONFIG_H
