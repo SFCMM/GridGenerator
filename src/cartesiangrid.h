faa1f0e6337562171b2033d4dd84ecf407eafa1c
svchb <berger.sven@gmail.com>
2021-07-21 10:47:54 +0200

    fix some missing constructors

--- src/cartesiangrid.h
+++ src/cartesiangrid.h
@@ -5,7 +5,12 @@
 
 class GridInterface {
  public:
-  virtual ~GridInterface() = default;
+  GridInterface()                     = default;
+  virtual ~GridInterface()            = default;
+  GridInterface(const GridInterface&) = delete;
+  GridInterface(GridInterface&&)      = delete;
+  auto operator=(const GridInterface&) -> GridInterface& = delete;
+  auto operator=(GridInterface&&) -> GridInterface& = delete;
 
   virtual void setBoundingBox(std::vector<GDouble> bbox) = 0;
 
@@ -15,7 +20,12 @@ class GridInterface {
 template <Debug_Level DebugLevel, GInt NDIM>
 class CartesianGrid : public GridInterface {
  public:
-  ~CartesianGrid() override = default;
+  CartesianGrid()                     = default;
+  ~CartesianGrid() override           = default;
+  CartesianGrid(const CartesianGrid&) = delete;
+  CartesianGrid(CartesianGrid&&)      = delete;
+  auto operator=(const CartesianGrid&) -> CartesianGrid& = delete;
+  auto operator=(CartesianGrid&&) -> CartesianGrid& = delete;
 
   void setBoundingBox(std::vector<GDouble> bbox) override {
     if(bbox.size() != 2 * NDIM) {
