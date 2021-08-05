7013e3a8f83d5a0e736255746c69f354665977c4
svchb <berger.sven@gmail.com>
2021-08-05 16:08:35 +0200

    some quick fixes

--- perf/perf.cpp
+++ perf/perf.cpp
@@ -1,50 +1,47 @@
-#include "math/hilbert.h"
 #include <benchmark/benchmark.h>
 #include <cmath>
 #include <cstring>
 #include <random>
+#include "math/hilbert.h"
 
-static void BM_Hilbert2D(benchmark::State &state) {
-  std::random_device r;
-  std::default_random_engine e1(r());
+static void BM_Hilbert2D(benchmark::State& state) {
+  std::random_device                     r;
+  std::default_random_engine             e1(r());
   std::uniform_real_distribution<double> uniform_dist(0, 1);
 
-  for (auto _ : state) {
-    state.PauseTiming();
-    VectorD<2> quadrant1 = {uniform_dist(e1), uniform_dist(e1)};
-    state.ResumeTiming();
-    hilbert::index<2>(quadrant1, state.range(0));
+  for(auto _ : state) {
+    //    state.PauseTiming(); // takes more time than the random num gen...
+    VectorD<2> quadrant1 = {uniform_dist(e1), uniform_dist(e1)}; // only relevant at hilbertLevel = 1
+    //    state.ResumeTiming(); // takes more time than the random num gen...
+    hilbert::index<2>(quadrant1, state.range(0)); //linear behaviour -> level * 1.2 ns
   }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_Hilbert2D)->DenseRange(1, 10, 1);
 
-static void BM_Hilbert3D(benchmark::State &state) {
-  std::random_device r;
-  std::default_random_engine e1(r());
+static void BM_Hilbert3D(benchmark::State& state) {
+  std::random_device                     r;
+  std::default_random_engine             e1(r());
   std::uniform_real_distribution<double> uniform_dist(0, 1);
 
-  for (auto _ : state) {
-    state.PauseTiming();
-    VectorD<3> quadrant1 = {uniform_dist(e1), uniform_dist(e1),
-                            uniform_dist(e1)};
-    state.ResumeTiming();
-    hilbert::index<3>(quadrant1, state.range(0));
+  for(auto _ : state) {
+    //    state.PauseTiming(); // takes more time than the random num gen...
+    VectorD<3> quadrant1 = {uniform_dist(e1), uniform_dist(e1), uniform_dist(e1)}; // only relevant at hilbertLevel = 1
+    //    state.ResumeTiming(); // takes more time than the random num gen...
+    hilbert::index<3>(quadrant1, state.range(0)); //linear behaviour -> level * 1.4 ns
   }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_Hilbert3D)->DenseRange(1, 10, 1);
 
-
 // example
-static void BM_memcpy(benchmark::State &state) {
-  char *src = new char[state.range(0)];
-  char *dst = new char[state.range(0)];
+static void BM_memcpy(benchmark::State& state) {
+  char* src = new char[state.range(0)];
+  char* dst = new char[state.range(0)];
   memset(src, 'x', state.range(0));
-  for (auto _ : state)
+  for(auto _ : state)
     memcpy(dst, src, state.range(0));
-  state.SetBytesProcessed(int64_t(state.iterations()) *
-                          int64_t(state.range(0)));
+  state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
   delete[] src;
   delete[] dst;
 }
