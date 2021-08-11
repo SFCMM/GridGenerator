238705cd923ab2dbeaf6195432f5ffeba80fa818
svchb <berger.sven@gmail.com>
2021-08-11 15:21:08 +0200

    refactor logging to header-only

--- src/log.h
+++ src/log.h
@@ -3,9 +3,9 @@
 #include <fstream>
 #include <memory>
 #include <mpi.h>
-#include <sfcmm_common.h>
 #include <sstream>
 #include <vector>
+#include <sfcmm_common.h>
 #include "config.h"
 #include "macros.h"
 
@@ -229,22 +229,24 @@ class Log_buffer : public std::stringbuf {
  *          for the MPI root domain (see #m_rootOnlyHardwired).
  */
 class Log_simpleFileBuffer : public Log_buffer {
- private:
-  GBool              m_isOpen{false};            //!< Stores whether the file(s) were already opened
-  GBool              m_rootOnlyHardwired{false}; //!< If true, only domain 0 opens and uses a file
-  GString            m_filename;                 //!< Filename on disk
-  std::ofstream      m_file;                     //!< File stream tied to physical file on disk
-  MPI_Comm           m_mpiComm{};                //!< MPI communicator group
-  std::ostringstream m_tmpBuffer;                //!< Temporary buffer to hold string until flushing
-
- protected:
-  auto sync() -> int override;
-  void flushBuffer() override;
-
  public:
   Log_simpleFileBuffer() = default;
-  Log_simpleFileBuffer(const GString& filename, const GInt m_argc, GChar** m_argv, MPI_Comm mpiComm = MPI_COMM_WORLD,
-                       GBool rootOnlyHardwired = false);
+
+  /**
+   * \brief This constructor yields a new instance that can immediately be used to write messages to a regular file.
+   * \details Internally, this constructor just passes the parameters to open() (see open() for more details).
+   *
+   * \param[in] filename          Filename that should be used for the file.
+   * \param[in] mpiComm           MPI communicator which is used to determine rank/domain information.
+   * \param[in] rootOnlyHardwired If true, only rank 0 creates a file and writes to it. On all other processors, no
+   *                              file is opened and at each flushing of the buffer, the buffer content is discarded.
+   */
+  Log_simpleFileBuffer(const GString& filename, const GInt argc, GChar** argv, MPI_Comm mpiComm = MPI_COMM_WORLD,
+                       GBool rootOnlyHardwired = false)
+    : Log_buffer(argc, argv) {
+    open(filename, mpiComm, rootOnlyHardwired);
+  }
+
   ~Log_simpleFileBuffer() override { Log_simpleFileBuffer::close(); };
 
   Log_simpleFileBuffer(const Log_simpleFileBuffer&) = delete;
@@ -252,8 +254,138 @@ class Log_simpleFileBuffer : public Log_buffer {
   auto operator=(const Log_simpleFileBuffer&) -> Log_simpleFileBuffer& = delete;
   auto operator=(Log_simpleFileBuffer&&) -> Log_simpleFileBuffer& = delete;
 
-  void open(const GString& filename, MPI_Comm mpiComm = MPI_COMM_WORLD, GBool rootOnlyHardwired = false);
-  void close(GBool forceClose = false) override;
+  /**
+   * \brief Initialization of the file I/O environment.
+   * \details After a successful call to this method the file stream is ready to use. Any previous information
+   *          written to the buffer is lost when open is called. This function creates a new file as specified in
+   *          filename (deleting any existing files with the same name), and creates the XML prefixes and suffixes
+   *          used for each message. It also writes the necessary XML header information to the file.
+   *
+   * \param[in] filename          Filename that should be used for the file.
+   * \param[in] mpiComm           MPI communicator which is used to determine rank/domain information.
+   * \param[in] rootOnlyHardwired If true, only rank 0 creates a file and writes to it. On all other processors, no
+   *                              file is opened and at each flushing of the buffer, the buffer content is discarded.
+   */
+  void open(const GString& filename, MPI_Comm mpiComm = MPI_COMM_WORLD, GBool rootOnlyHardwired = false) {
+    // Open file only if it was not yet done
+    if(!m_isOpen) {
+      // Set MPI communicator group
+      m_mpiComm = mpiComm;
+
+      // Get domain id and number of domains
+      MPI_Comm_rank(m_mpiComm, &domainId());
+      MPI_Comm_size(m_mpiComm, &noDomains());
+
+      // Set whether only domain 0 should do any writing (including the creation of a file)
+      m_rootOnlyHardwired = rootOnlyHardwired;
+
+      // Only open the file if m_rootOnlyHardwired was not set as true. Otherwise the file state remains closed.
+      if(!(m_rootOnlyHardwired && domainId() != 0)) {
+        // Set filename
+        m_filename = filename;
+
+        // Open file
+        m_file.open(m_filename.c_str());
+
+        // Clear internal buffer in order to dismiss any previous input
+        str("");
+
+        // Create prefix and suffix messages
+        createPrefixMessage();
+        createSuffixMessage();
+
+        // Write root and meta information to file
+        m_file << getXmlHeader() << std::flush;
+
+        // Set state variable
+        m_isOpen = true;
+      }
+    }
+  }
+
+  /**
+   * \brief Closes the file.
+   * \details Any subsequent write statements to the file stream are discarded after this method is called. After
+   *          close() is called, an XML footer is written to the file. Then the file is closed.
+   */
+  void close(GBool forceClose = false) override {
+    // forceClose is not needed here (only kept for interface consistency reasons)
+    static_cast<void>(forceClose);
+
+    // Only close file if was opened before
+    if(m_isOpen) {
+      // Force flushing of the internal buffer
+      flushBuffer();
+
+      // Write XML footer to file and flush stream
+      m_file << getXmlFooter() << std::flush;
+
+      // Close file stream
+      m_file.close();
+
+      // Set state variable
+      m_isOpen = false;
+    }
+  }
+
+ protected:
+  /**
+   * \brief Flushes the buffer by writing the contents to the file.
+   * \details Sync is called automatically when an "endl" is sent to the stream. At first the buffer content is
+   *          wrapped in the prefix and suffix messages, then the entire string is written to the file by calling
+   *          flushBuffer(). Finally, the internal buffers are reset.
+   *
+   * \return Zero by default.
+   */
+  auto sync() -> int override {
+    // Only write if the file was already opened
+    if(m_isOpen) {
+      // Create formatted string, escape any XML entities in the message, and save to temporary buffer
+      m_tmpBuffer << prefixMessage() << encodeXml(str()) << suffixMessage();
+
+      // Only write to file if current buffer length exceeds the minimum size for flushing
+      if(m_tmpBuffer.str().length() >= static_cast<unsigned>(minFlushSize())) {
+        // Write the string to the file and flush the stream
+        m_file << m_tmpBuffer.str() << std::flush;
+
+        // Reset temporary buffer
+        m_tmpBuffer.str("");
+      }
+    }
+
+    // Reset internal buffer
+    str("");
+
+    // Default return value for sync()
+    return 0;
+  }
+
+  /**
+   * \brief Flushes the buffer by writing the contents to the file.
+   * \details Sync is called automatically when an "endl" is sent to the stream. At first the buffer content is
+   *          wrapped in the prefix and suffix messages, then the entire string is written to the file. Finally,
+   *          the internal buffers are reset.
+   *
+   * \return Zero by default.
+   */
+  void flushBuffer() override {
+    // Only write if the file was already opened
+    if(m_isOpen) {
+      // Write the string to the file and flush the stream
+      m_file << m_tmpBuffer.str() << std::flush;
+
+      // Reset temporary buffer
+      m_tmpBuffer.str("");
+    }
+  }
+
+ private:
+  GBool              m_isOpen{false};            //!< Stores whether the file(s) were already opened
+  GBool              m_rootOnlyHardwired{false}; //!< If true, only domain 0 opens and uses a file
+  GString            m_filename;                 //!< Filename on disk
+  std::ofstream      m_file;                     //!< File stream tied to physical file on disk
+  MPI_Comm           m_mpiComm{};                //!< MPI communicator group
+  std::ostringstream m_tmpBuffer;                //!< Temporary buffer to hold string until flushing
 };
 
 /**
@@ -277,9 +409,6 @@ class Log : public std::ostream {
   virtual auto setRootOnly(GBool rootOnly = true) -> GBool = 0;
 
   /** \brief Adds an attribute to the prefix of the XML string.
-   * \author Andreas Lintermann, Sven Berger
-   * \date 13.08.2012
-   *
    * \param[in] att The attribute to add, consists of a pair of MStrings.
    * \return The location of the attribute in the vector of pairs.
    */
@@ -290,9 +419,6 @@ class Log : public std::ostream {
   }
 
   /** \brief Erases an attribute from the prefix of the XML string.
-   * \author Andreas Lintermann, Sven Berger
-   * \date 13.08.2012
-   *
    *  \param[in] attId The ID of the attribute to delete.
    */
   void eraseAttribute(GInt attId) {
@@ -301,9 +427,6 @@ class Log : public std::ostream {
   }
 
   /** \brief Modifies an attribute of the prefix of the XML string.
-   * \author Andreas Lintermann, Sven Berger
-   * \date 13.08.2012
-   *
    *  \param[in] attId The ID of the attribute to modify.
    *  \param[in] att The new attribute to replace the old one, given by a pair of MStrings.
    */
@@ -321,8 +444,6 @@ class Log : public std::ostream {
 
 /**
  * \brief Class to create an output stream for a writable file, using a physical file.
- * \author Michael Schlottke, Sven Berger
- * \date June 2012
  * \details This class can be used to open a file on all processors (alternatively: only on a specified MPI
  *          communicator) and write to it using the normal C++ stream syntax (i.e. just like cout or cerr).
  *
@@ -331,14 +452,10 @@ class Log : public std::ostream {
  *          within an MPI communicator opens a file to write to.
  */
 class LogFile : public Log {
- private:
-  bool m_isOpen = false; //!< Stores whether a file was already opened or not
-
  public:
   LogFile() = default;
   /**
    * \brief Constructor creates LogFile and calls ostream constructor with reference to it.
-   * \author Sven Berger
    * \details When this constructor is used, the stream is immediately ready to use. For information about the
    * parameters, please have a look at Log*Buffer::open.
    *
@@ -355,10 +472,71 @@ class LogFile : public Log {
   auto operator=(const LogFile&) -> LogFile& = delete;
   auto operator=(LogFile&&) -> LogFile& = delete;
 
-  void open(const GString& filename, const GBool rootOnlyHardwired, const GInt argc, GChar** argv, MPI_Comm mpiComm = MPI_COMM_WORLD);
-  void close(GBool forceClose = false);
-  auto setRootOnly(GBool rootOnly = true) -> GBool override;
-  auto setMinFlushSize(GInt minFlushSize) -> GInt;
+  /**
+   * \brief Opens a file by passing the parameters to Log_<xyz>FileBuffer::open(...).
+   * \details The method then creates a new internal buffer and passes along the parameters.
+   *
+   * \param[in] filename Name of the file to open.
+   * \param[in] mpiComm MPI communicator for which to open the file.
+   * \param[in] rootOnlyHardwired If true, only rank 0 creates a file and writes to it. On all other processors, no
+   *                              file is opened and at each flushing of the buffer, the buffer content is discarded.
+   */
+  void open(const GString& filename, const GBool rootOnlyHardwired, const GInt argc, GChar** argv, MPI_Comm mpiComm = MPI_COMM_WORLD) {
+    // Only open file if it was not yet opened
+    if(!m_isOpen) {
+      // Open a simple file
+      buffer() = std::make_unique<Log_simpleFileBuffer>(filename, argc, argv, mpiComm, rootOnlyHardwired);
+
+      // Associate the stream with the newly created buffer
+      rdbuf(buffer().get());
+
+      // Set state variable
+      m_isOpen = true;
+    }
+  }
+
+  /**
+   * \brief Pass the close call to the respective internal buffer.
+   * \details All attempts to write to the stream after closing it will be discarded.
+   */
+  void close(GBool forceClose = false) {
+    // Only close file if was already opened
+    if(m_isOpen) {
+      buffer()->close(forceClose); // NOLINT(cppcoreguidelines-pro-type-static-cast-downcast)
+
+      // Delete internal buffer to prevent memory leaks
+      buffer().reset();
+
+      // Set state variable
+      m_isOpen = false;
+    }
+  }
+
+  /**
+   * \brief Sets interal state of whether only the root domain (rank 0) should write to file.
+   * \params[in] rootOnly If true, only rank 0 of the specified MPI communicator writes to file.
+   * \return The previous internal state (may be stored to return to the previous behavior).
+   */
+  auto setRootOnly(GBool rootOnly = true) -> GBool override { return buffer()->setRootOnly(rootOnly); }
+
+  /**
+   * \brief Sets the minimum buffer length that has to be reached before the buffer is flushed.
+   * \details Flushing the buffer means that the contents of the buffer in memory is written to the file. If the
+   *          file stream was not opened yet, this method just returns 0 and does nothing else.
+   *
+   * \params[in] minFlushSize Minimum buffer length.
+   * \return The previous value of the minimum flush size.
+   */
+  auto setMinFlushSize(GInt minFlushSize) -> GInt {
+    if(m_isOpen) {
+      return buffer()->setMinFlushSize(minFlushSize);
+    }
+
+    return 0;
+  }
+
+ private:
+  bool m_isOpen = false; //!< Stores whether a file was already opened or not
 };
 inline LogFile logger; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
