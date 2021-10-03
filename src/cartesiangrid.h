b4d1d2ade989fd58317c79a32ca9e1debec9d98a
svchb <berger.sven@gmail.com>
2021-10-03 20:59:45 +0200

    copy grid to lbm solver

--- src/cartesiangrid.h
+++ src/cartesiangrid.h
@@ -5,11 +5,15 @@
 
 #include <sfcmm_common.h>
 #include "base_cartesiangrid.h"
-#include "cartesiangrid_generation.h"
 #include "celltree.h"
 #include "common/IO.h"
 #include "geometry.h"
 #include "globaltimers.h"
+#ifdef SOLVER_AVAILABLE
+#include "gridgenerator/cartesiangrid_generation.h"
+#else
+#include "cartesiangrid_generation.h"
+#endif
 #include "interface/grid_interface.h"
 
 template <Debug_Level DEBUG_LEVEL, GInt NDIM>
@@ -26,7 +30,7 @@ class CartesianGrid : public BaseCartesianGrid<DEBUG_LEVEL, NDIM> {
     if(!m_tree.empty()) {
       TERMM(-1, "Invalid operation tree already allocated.");
     }
-    m_tree.reset(capacity);
+    m_tree.setCapacity(capacity);
   }
 
   void reset() override { m_tree.reset(); }
@@ -37,10 +41,48 @@ class CartesianGrid : public BaseCartesianGrid<DEBUG_LEVEL, NDIM> {
   /// \param grid Generated grid.
   void loadGridInplace(const CartesianGridGen<DEBUG_LEVEL, NDIM>& grid) {
     // grid.balance(); //todo: implement
+    m_tree.setCapacity(grid.capacity()); // todo: change for adaptation
+
+#ifdef _OPENMP
+#pragma omp parallel default(none) shared(grid)
+    {
+#endif
+      const GInt noCells = grid.size();
+#ifdef _OPENMP
+#pragma omp for
+#endif
+      for(GInt cellId = 0; cellId < noCells; ++cellId) {
+        m_tree.globalId(cellId) = grid.globalId(cellId);
+        m_tree.parent(cellId)   = grid.parent(cellId);
+        for(GInt childId = 0; childId < cartesian::maxNoChildren<NDIM>(); ++childId) {
+          m_tree.child(cellId, childId) = grid.child(cellId, childId);
+        }
+        for(GInt nghbrId = 0; nghbrId < cartesian::maxNoNghbrs<NDIM>(); ++nghbrId) {
+          m_tree.neighbor(cellId, nghbrId) = grid.neighbor(cellId, nghbrId);
+        }
+        m_tree.level(cellId) = grid.level(cellId);
+        for(GInt dir = 0; dir < NDIM; ++dir) {
+          m_tree.center(cellId, dir) = grid.center(cellId, dir);
+        }
+      }
+#ifdef _OPENMP
+    }
+#endif
+    setProperties();
+    if(m_loadBalancing) {
+      setWorkload();
+      calculateOffspringsAndWeights();
+    }
   }
 
  private:
-  cartesian::Tree<DEBUG_LEVEL, NDIM> m_tree;
+  void setProperties(){};
+  void setWorkload() { TERMM(-1, "Not implemented!"); };
+  void calculateOffspringsAndWeights() { TERMM(-1, "Not implemented!"); };
+
+  cartesian::Tree<DEBUG_LEVEL, NDIM> m_tree{};
+
+  GBool m_loadBalancing = false;
 };
 
 #endif // GRIDGENERATOR_CARTESIANGRID_H
