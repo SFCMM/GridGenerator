1e8a915162348a4299255f0c48ad151c46523dd4
svchb <berger.sven@gmail.com>
2021-12-03 15:18:34 +0100

    reformat

--- src/gridGenerator.cpp
+++ src/gridGenerator.cpp
@@ -41,7 +41,7 @@ void GridGenerator<DEBUG_LEVEL>::init(int argc, GChar** argv, GString config_fil
 
 template <Debug_Level DEBUG_LEVEL>
 void GridGenerator<DEBUG_LEVEL>::initBenchmark(int argc, GChar** argv) {
-  m_benchmark             = true;
+  m_benchmark = true;
 
   m_dim              = 3;
   m_maxNoCells       = 100000;
