f59f834dfc56b75ece44ef3e2bc976ac6954840f
svchb <berger.sven@gmail.com>
2022-03-22 02:15:23 +0100

    merge

--- src/common/surface.h
+++ src/common/surface.h
@@ -10,8 +10,9 @@ class SurfaceInterface {
   virtual void               setCellList(const std::vector<GInt>& cellList)  = 0;
   virtual void               addCell(const GInt cellId, const GInt dir)      = 0;
 
-  [[nodiscard]] virtual auto normal_p(const GInt surfCellId) const -> const GDouble*   = 0;
-  [[nodiscard]] virtual auto neighbor(const GInt cellId, const GInt dir) const -> GInt = 0;
+  [[nodiscard]] virtual auto normal_p(const GInt surfCellId) const -> const GDouble*               = 0;
+  [[nodiscard]] virtual auto neighbor(const GInt cellId, const GInt dir) const -> GInt             = 0;
+  [[nodiscard]] virtual auto property(const GInt cellId, const CellProperties prop) const -> GBool = 0;
 
  private:
 };
@@ -20,11 +21,13 @@ class SurfaceInterface {
 template <Debug_Level DEBUG_LEVEL, GInt NDIM>
 class Surface : public SurfaceInterface {
  public:
-  Surface(CartesianGridData<NDIM> data) : m_grid(data){};
+  explicit Surface(CartesianGridData<NDIM> data, grid::cell::BitsetType* properties) : m_grid(data), m_properties(properties){};
   ~Surface() = default;
 
+  Surface(const Surface<DEBUG_LEVEL, NDIM>& copy) = default;
+
+
   // todo:fix
-  //  Surface(const Surface& copy) : m_center(copy.m_center), m_grid(copy.m_grid) {}
   //  Surface(Surface&&)      = delete;
   //    auto operator=(const Surface& copy) -> Surface& {
   //      m_center = copy.m_center;
@@ -43,11 +46,11 @@ class Surface : public SurfaceInterface {
     }
 
     m_cellId.clear();
-    m_nghbrIds.clear();
+    //    m_nghbrIds.clear();
     std::copy(cellList.begin(), cellList.end(), std::back_inserter(m_cellId));
 
 
-    updateNeighbors();
+    //    updateNeighbors();
   }
 
   void addCell(const GInt cellId, const GInt dir) override {
@@ -60,23 +63,12 @@ class Surface : public SurfaceInterface {
     m_normal.erase(cellId);
   }
 
-  void updateNeighbors() {
-    for(const auto cellId : m_cellId) {
-      std::array<GInt, cartesian::maxNoNghbrsDiag<NDIM>()> tmpNghbr;
-      for(GInt nghbrId = 0; nghbrId < cartesian::maxNoNghbrsDiag<NDIM>(); ++nghbrId) {
-        tmpNghbr[nghbrId] = m_grid.neighbor(cellId, nghbrId);
-      }
-      m_nghbrIds.insert({cellId, tmpNghbr});
-    }
-  }
-
   auto normal(const GInt surfCellId) const -> const VectorD<NDIM>& { return m_normal.at(surfCellId); }
 
   [[nodiscard]] auto normal_p(const GInt surfCellId) const -> const GDouble* override { return &m_normal.at(surfCellId)[0]; }
 
 
   [[nodiscard]] auto size() const -> GInt {
-    ASSERT(!m_cellId.empty(), "Not inited!");
     return m_cellId.size();
   }
 
@@ -87,13 +79,24 @@ class Surface : public SurfaceInterface {
   auto grid() const -> CartesianGridData<NDIM> { return m_grid; }
 
   [[nodiscard]] auto neighbor(const GInt cellId, const GInt dir) const -> GInt override {
+    //    if(DEBUG_LEVEL >= Debug_Level::debug) {
+    //      if(m_nghbrIds.empty()) {
+    //        TERMM(-1, "Surface not initialized!");
+    //      }
+    //    }
+
     if(DEBUG_LEVEL >= Debug_Level::debug) {
-      if(m_nghbrIds.empty()) {
-        TERMM(-1, "Surface not initialized!");
+      //      if(m_nghbrIds.count(cellId) == 0){
+      //        cerr0<<"ERROR: Invalid cellId " << cellId << std::endl;
+      //        std::exit(-1);
+      //      }
+      if(dir >= cartesian::maxNoNghbrsDiag<NDIM>()) {
+        cerr0 << "ERROR: Invalid direction for " << cellId << " and " << dir << std::endl;
+        std::exit(-1);
       }
     }
-
-    return m_nghbrIds.at(cellId)[dir];
+    return m_grid.neighbor(cellId, dir);
+    //    return m_nghbrIds.at(cellId)[dir];
   }
 
   // todo: probably not needed
@@ -102,12 +105,24 @@ class Surface : public SurfaceInterface {
   // todo: probably not needed
   [[nodiscard]] auto hasBndryGhostCells() const -> GBool { return m_hasBndryGhosts; }
 
+  [[nodiscard]] auto property(const GInt cellId, const CellProperties prop) const -> GBool override {
+    return m_grid.property(cellId, prop);
+  }
+
+  void property(const GInt cellId, const CellProperties prop, const GBool value) { m_properties[cellId][static_cast<GInt>(prop)] = value; }
+
+  auto setProperty(const CellProperties prop, const GBool value) {
+    for(const GInt surfCellId : m_cellId) {
+      property(surfCellId, prop, value);
+    }
+  }
+
  private:
-  std::vector<GInt>                                                              m_cellId;
-  std::unordered_map<GInt, std::array<GInt, cartesian::maxNoNghbrsDiag<NDIM>()>> m_nghbrIds;
+  std::vector<GInt> m_cellId;
 
   std::unordered_map<GInt, VectorD<NDIM>> m_normal;
-  const CartesianGridData<NDIM>           m_grid = nullptr;
+  CartesianGridData<NDIM>                 m_grid;
+  grid::cell::BitsetType*                 m_properties = nullptr;
 
   GBool m_hasBndryGhosts = false;
 };
